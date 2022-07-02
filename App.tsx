98d3ea9a8b59e57d11c2f371e8b7aefb3b94a247
ghkatende <ghkatende@gmail.com>
2022-07-02 10:17:02 +0300

    Added the App.tsx file

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {store, persistor} from './src/redux/store';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import Navigation from './src/navigation/Navigation';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Navigation />
+      </PersistGate>
+    </Provider>
+  );
+};
+
+export default App;
